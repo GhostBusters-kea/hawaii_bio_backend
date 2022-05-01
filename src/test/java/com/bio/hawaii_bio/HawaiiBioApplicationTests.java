8bf9302dc3dd3eff2ea4b6ee551e267344cc9ec6
jmunkc <jona04g8@stud.kea.dk>
2022-05-01 21:13:56 +0200

    init

---
+++ src/test/java/com/bio/hawaii_bio/HawaiiBioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bio.hawaii_bio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HawaiiBioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
