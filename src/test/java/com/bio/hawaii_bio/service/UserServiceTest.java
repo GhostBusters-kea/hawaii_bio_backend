d73930ee1a6b60ec0a1476269566f8b46c0d675f
jmunkc <jona04g8@stud.kea.dk>
2022-05-10 23:31:26 +0200

    added some userApi Test

--- src/test/java/com/bio/hawaii_bio/service/UserServiceTest.java
+++ src/test/java/com/bio/hawaii_bio/service/UserServiceTest.java
@@ -10,12 +10,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ActiveProfiles("test")
 @DataJpaTest
 class UserServiceTest {
 
