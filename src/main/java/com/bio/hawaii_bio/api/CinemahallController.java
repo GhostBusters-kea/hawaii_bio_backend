eb16838dc773f6d96053fbcfd8e18cba3b2ad3f4
MichaelDyvad <michaeldyvad@gmail.com>
2022-05-15 14:08:33 +0200

    lessgo

--- src/main/java/com/bio/hawaii_bio/api/CinemahallController.java
+++ src/main/java/com/bio/hawaii_bio/api/CinemahallController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @CrossOrigin
-@Controller
+@RestController
 @RequestMapping("api/cinemahall")
 public class CinemahallController {
     CinemaHallService cinemaHallService;
--- src/main/java/com/bio/hawaii_bio/entity/CinemaHall.java
+++ src/main/java/com/bio/hawaii_bio/entity/CinemaHall.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -20,10 +21,17 @@ public class CinemaHall {
 
     private int seatsReserved;
 
+    @Column(nullable = false, columnDefinition = "TINYINT(1)")
+    private int a1;
+
+    @Column(nullable = false, columnDefinition = "TINYINT(1)")
+    private int a2;
+
     public CinemaHall(CinemaHallRequest body){
         this.id = body.getId();
         this.seats = body.getSeats();
         this.seatsReserved = body.getSeatsReserved();
+        this.a1 = body.getA1();
+        this.a2 = body.getA2();
     }
-
 }
