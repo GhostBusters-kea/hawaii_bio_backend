d97211bec20517a73b28eaf9976b589cdb0d8712
Lars <larsbrogaard86@gmail.com>
2022-05-19 10:08:41 +0200

    annotering på MovieRepo

--- src/main/java/com/bio/hawaii_bio/repo/MovieRepo.java
+++ src/main/java/com/bio/hawaii_bio/repo/MovieRepo.java
@@ -3,9 +3,13 @@ package com.bio.hawaii_bio.repo;
 import com.bio.hawaii_bio.entity.Category;
 import com.bio.hawaii_bio.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Repository
+@Transactional
 public interface MovieRepo extends JpaRepository<Movie, Integer> {
 
     List<Movie> findMoviesByCategory(Category category);
